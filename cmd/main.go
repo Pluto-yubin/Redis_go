3fbf40568eb718c69886f07fbf12638700cf6918
Pluto-yubin <1113940328@qq.com>
2021-03-30 00:05:49 +0800

    test voteArticle successful

--- cmd/main.go
+++ cmd/main.go
@@ -12,11 +12,10 @@ func main() {
 
 	client := conf.GetRedisClient()
 	var article1, article2 *model.Article
-	article2 = &model.Article{}
-	article1 = &model.Article{}
 	server.PostArticle(client, article1, "java", "1", "java.com", "小王",1,time.Now().Unix() - model.ONE_WEEK_IN_SECONDS - 1)
 	server.PostArticle(client, article2, "go", "2", "golang.com", "小红", 1, time.Now().Unix())
 	article := server.GetArticle(client, 0, "time")
 	fmt.Println(article)
-
+	val := server.ArticleVote(client, "1", "1")
+	fmt.Println(val)
 }
